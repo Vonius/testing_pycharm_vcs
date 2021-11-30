2542e5de434c7c5fe5608c6b5b8656328ed7e6a3
Vonius <ivankudryashev@gmail.com>
2021-11-30 19:58:57 +0100

    #2 simple branch created

---
+++ functions.py
@@ -0,0 +1,2 @@
+def call_my_name():
+    print("hi! i'm pipo")
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
+from functions import call_my_name
+
+
 def main(a, b):
     print(f"The sum of {a} and {b} is {a+b}")
     return a + b
 
 
 if __name__ == "__main__":
-    main(4,6)
+    main(4, 6)
+    call_my_name()
